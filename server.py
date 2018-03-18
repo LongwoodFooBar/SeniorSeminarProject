294324792ad451a7bb141c6bb1b242ae615f11cc
Jacob Carney <jacobsc897@gmail.com>
2018-03-18 16:23:00 -0400

    Added preliminary signin code

--- server.py
+++ server.py
@@ -1,14 +1,39 @@
 from flask import Flask, g, render_template, request, redirect, url_for, escape, session
 import sqlite3
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(
+    SECRET_KEY="foobardongle"
+))
+
+def connect_db():
+    pass
+
+def get_db():
+    pass
 
 @app.route('/')
 def root():
+    if 'username' in session:
+        return redirect(url_for("courses"))
     return redirect(url_for("login"))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        error = None
+        validlogin = False
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+        if session['username'] == "admin" and session['password'] == "FooBar":
+            validlogin = True
+
+        if validlogin:
+            return redirect(url_for('courses'))
+        else:
+            error = "Incorrect username or password"
+            return render_template('login.html', loginerror=error)
+    return render_template('login.html')
     #return "Login Page"
 
 @app.route('/signup')
@@ -17,7 +42,9 @@ def signup():
 
 @app.route('/logout')
 def logout():
-    return "Logout Page"
+    session.pop('username', None)
+    return redirect(url_for('login'))
+    #return "Logout Page"
 
 @app.route('/courses')
 def courses():
