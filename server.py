017f30fc811f55011590df61ed0250b18b57c69d
Jacob Carney <jacobsc897@gmail.com>
2018-03-29 22:13:47 -0400

    Fixed student courses

--- server.py
+++ server.py
@@ -98,7 +98,7 @@ def courses():
 			return render_template('courses.html', user=session['username'], courses=cs)
 			
 		elif utype[0][0] == 'STUDENT':
-			cs = db.execute("SELECT title FROM class JOIN login on class.instructorID=login.userID WHERE login.email=?", (session['username'],)).fetchall()
+			cs = db.execute("SELECT title FROM class NATURAL JOIN takes NATURAL JOIN login WHERE login.email=?", (session['username'],)).fetchall()
 			for i in range(len(cs)):
 				cs[i] = list(cs[i])
 				cs[i].append(db.execute("SELECT assignment.title FROM assignment JOIN class ON class.classID=assignment.classID WHERE class.title=?", (cs[i][0],)).fetchall())
