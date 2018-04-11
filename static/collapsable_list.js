a2808b81a5fbf284fcba7a29ecdb498404b2212f
Heather Switzer <heather.switzer@live.longwood.edu>
2018-04-11 19:06:46 -0400

    A completely useless about the team page

--- static/collapsable_list.js
+++ static/collapsable_list.js
@@ -30,6 +30,20 @@ for (i = 0; i < coll.length; i++) {
 });
 }	
 
+var coll = document.getElementsByClassName("memcollapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function() {
+	this.classList.toggle("active");
+	var content = this.nextElementSibling;
+	if (content.style.display === "block") {
+		content.style.display = "none";
+	} 
+	else {
+		content.style.display = "block";
+	}
+});
+}
 
 
